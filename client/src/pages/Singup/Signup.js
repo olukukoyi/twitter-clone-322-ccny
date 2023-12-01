f20bb34737ca339b74c43695a71832950319a86a
Ramim Tarafdar <rtarafdar96@gmail.com>
2023-12-01 18:15:15 -0500

    Updated all files

--- client/src/pages/Singup/Signup.js
+++ client/src/pages/Singup/Signup.js
@@ -1,8 +1,8 @@
 import React from "react";
-import "./Login.css";
+import "./Signup.css";
 
 
-const Login = () => {
+const Signup = () => {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
     const [login, setLogin] = useState(false);
@@ -13,4 +13,4 @@ const Login = () => {
     };
 };
 
-export default Login;
+export default Signup;
