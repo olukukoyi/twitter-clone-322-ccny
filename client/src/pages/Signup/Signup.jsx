e0411f51e891c453d6cb43f9ae17f767a7a36a39
Ramim Tarafdar <rtarafdar96@gmail.com>
2023-12-03 02:08:59 -0500

    Updated Signup Page
    (missing db connection)

--- client/src/pages/Signup/Signup.jsx
+++ client/src/pages/Signup/Signup.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Signup.css";
+import Vlogo from "../../images/vlogo.png";
+
 
 
 const Signup = () => {
@@ -9,60 +12,73 @@ const Signup = () => {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
     const [selectedUserType, setSelectedUserType] = useState('');
+
+    const addNewUser = (e) => {
+        e.preventDefault();
+        console.log(selectedUserType);
+    
+    }
   
     return (
-      <div className="signup">
-        <div className="signup-logo">
-          
-        </div>
-        <div className="signup-form">
-            <h1>Happening Now</h1>
-            <h2 className="form-subtitle">Join today.</h2>
+        <div className="signup">
+            <div className="signup-left">
+                <img className="signup-left-logo" src={Vlogo} alt="vlogo" />
+            </div>
+            <div className="signup-right">
+                <h1 className="signup-right-title">Happening now</h1>
+                <h2 className="signup-right-subtitle">Join today.</h2>
+                <form onSubmit={addNewUser} className="signup-form">
+                    <div className="form-group">
+                        <input
+                        type="text"
+                        className="form-input"
+                        placeholder="First Name"
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                        type="text"
+                        className="form-input"
+                        placeholder="Last Name"
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                        type="text"
+                        className="form-input"
+                        placeholder="Username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                        type="email"
+                        className="form-input"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                        type="password"
+                        className="form-input"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <button type="submit" className="signup-button">Sign Up</button>
 
-          <form onSubmit={addNewUser} className="signup-form">
-            <input
-              type="text"
-              className="form-input"
-              placeholder="First Name"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-            <input
-              type="text"
-              className="form-input"
-              placeholder="Last Name"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-            />
-            <input
-              type="text"
-              className="form-input"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            
-            <input
-              type="email"
-              className="form-input"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-  
-            <input
-              type="password"
-              className="form-input"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            
-            
-            <button type="submit" className="signup-button">Sign Up</button>
-          </form>
+                    <p>Already have an account? Log In </p>
+                </form>
+            </div>
         </div>
-      </div>
     );
   };
 export default Signup;
