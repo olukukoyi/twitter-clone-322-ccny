243a5c5240c111e68cb196971c4d97bb04e98f46
Ramim Tarafdar <rtarafdar96@gmail.com>
2023-12-03 02:08:11 -0500

    Updated Login Page
    (missing db connection)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,25 @@
-
-import Signup from './pages/Signup/Signup'; 
-/*
 import { Routes, Route, Link, BrowserRouter } from 'react-router-dom';
-import Homepage from './pages/Homepage/Homepage';
 import Login from './pages/Login/Login';
-
-import Profile from './pages/Profile/Profile';
-import Settings from './pages/Settings/Settings';
-*/
+import Signup from './pages/Signup/Signup';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Login />
     </div>
-
+    
+    
     /*
-    <div className="app">
-      <BrowserRouter>
+    <BrowserRouter>
+      <div className="App">
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/settings" element={<Settings />} />
         </Routes>
-      </BrowserRouter>
-    </div>
+      </div>
+    </BrowserRouter>
     */
   );
 }
--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,16 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Login.css";
+import Vlogo from "../../images/vlogo.png";
+
 
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email)
+    }
 
     return (
         <div className="login">
-            <div className="login-logo">
-                <img src="/images/vlogo.png" alt="" />
+            <div className="login-left">
+                <img className="login-left-logo" src={Vlogo} alt="vlogo" />
             </div>
-            <div className="login-form">
+            <div className="login-right">
+                <h1 className="login-right-title">Happening now</h1>
+                <h2 className="login-right-subtitle">Join today.</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <input
+                            type="email"
+                            className="form-input"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input 
+                            type="password"
+                            className="form-input"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <button type="submit" className="login-button">Login</button>
+                </form>
 
+                <p>Don't have an account? Sign Up </p>
             </div>
         </div>
     );
