2c6cc4de44923bac71cc3240629cd316452f621b
Mahmud Hasan <28367258+QuodFinis@users.noreply.github.com>
2023-12-04 22:50:21 -0500

    added homepage stuff from my branch

--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from 'react';
+import Feed from './Feed.jsx';
+import TweetInput from './TweetInput.jsx';
+import Sidebar from './Sidebar.jsx';
+import './HomePage.css';
 
-export const Homepage = () => {
-  return (
-    <div>Homepage</div>
-  )
+function HomePage() {
+    // TODO: Replace with actual data
+    const tweets = ['Tweet1', 'Tweet2', 'Tweet3'];
+    const trendingUsers = [
+        { username: 'User1' },
+        { username: 'User2' },
+        { username: 'User3' },
+    ];
+
+    const handleTweet = (tweet) => {
+        console.log('New tweet:', tweet);
+        // TODO: logic to handle new tweet
+    };
+
+    return (
+        <div className="homepage">
+            <div className="tweet-input">
+                <TweetInput onTweet={handleTweet} />
+            </div>
+            <div className="feed">
+                <Feed tweets={tweets} />
+            </div>
+            <div className="sidebar">
+                <Sidebar trendingUsers={trendingUsers} />
+            </div>
+        </div>
+    );
 }
+
+export default HomePage;
