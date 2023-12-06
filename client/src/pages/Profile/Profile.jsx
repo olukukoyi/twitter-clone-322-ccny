ca7f4e427ad5cd3b33fd926081003eaecfb6bd23
Mahmud Hasan <28367258+QuodFinis@users.noreply.github.com>
2023-12-05 20:37:56 -0500

    added payment profile and settings page to ramim branch

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,7 +1,143 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-export const Profile = () => {
+const ProfilePage = () => {
+  const [showImagePopup, setShowImagePopup] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch('http://localhost:8001/user/:id'); // Replace ':id' with the actual user ID
+        const fetchedUserData = await response.json();
+        setUserData(fetchedUserData);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+    setShowImagePopup(true);
+  };
+
+  const handlePopupClose = () => {
+    setShowImagePopup(false);
+  };
+
+  const handleImageChange = () => {
+    console.log('Change image functionality');
+  };
+
   return (
-    <div>Profile</div>
-  )
-}
+      <div>
+        <div style={styles.backgroundImageContainer} onClick={() => handleImageClick(userData.backgroundImage)}>
+          <div style={styles.profileInfoContainer}>
+            <img
+                src={userData.profilePicture}
+                alt="Profile"
+                style={styles.profilePicture}
+                onClick={() => handleImageClick(userData.profilePicture)}
+            />
+            <div style={styles.profileDetails}>
+              <h2>{userData.username}</h2>
+              <p>{userData.bio}</p>
+              <div style={styles.stats}>
+                <div>{userData.tweetCount} Tweets</div>
+                <div>{userData.followers} Followers</div>
+                <div>{userData.following} Following</div>
+                <div>{userData.ads} Ads</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {userData.tweets.map((tweet) => (
+            <div key={tweet.id} style={styles.tweetContainer}>
+              <p>{tweet.content}</p>
+            </div>
+        ))}
+
+        {showImagePopup && (
+            <div style={styles.imagePopup}>
+          <span style={styles.closeButton} onClick={handlePopupClose}>
+            &times;
+          </span>
+              <img src={selectedImage} alt="Popup" style={styles.popupImage} />
+              <div style={styles.changeImageButton} onClick={handleImageChange}>
+                Change Image
+              </div>
+            </div>
+        )}
+      </div>
+  );
+};
+
+const styles = {
+  backgroundImageContainer: {
+    height: '50vh',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  profileInfoContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    textAlign: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+    padding: '20px',
+    borderRadius: '10px',
+  },
+  profilePicture: {
+    width: '100px',
+    height: '100px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+  },
+  profileDetails: {
+    marginTop: '10px',
+  },
+  stats: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    marginTop: '10px',
+  },
+  tweetContainer: {
+    border: '1px solid #ddd',
+    borderRadius: '8px',
+    margin: '10px',
+    padding: '10px',
+  },
+  imagePopup: {
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+    padding: '20px',
+    borderRadius: '10px',
+    zIndex: 1000,
+    cursor: 'pointer',
+  },
+  popupImage: {
+    maxWidth: '100%',
+    maxHeight: '80vh',
+    borderRadius: '8px',
+  },
+  changeImageButton: {
+    position: 'absolute',
+    bottom: '10px',
+    left: '10px',
+    cursor: 'pointer',
+    color: '#1B95E0',
+  },
+};
+
+export default ProfilePage;
--- client/src/pages/Settings/Settings.jsx
+++ client/src/pages/Settings/Settings.jsx
@@ -1,7 +1,123 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-export const Settings = () => {
-  return (
-    <div>Settings</div>
-  )
-}
+const Settings = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [accountType, setAccountType] = useState('normal');
+    const [bankAccount, setBankAccount] = useState('');
+
+    // Simulating fetching user data from a backend
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                // Simulating a backend response
+                const response = await fetch('/api/user'); // replace with your actual endpoint
+                const userData = await response.json();
+
+                // Setting state with fetched user data
+                setUsername(userData.username);
+                setEmail(userData.email);
+                // Password is usually not fetched from the server for security reasons
+                // If you need to display some information about the password, consider not fetching it from the server
+                // and let the user update it separately if needed.
+
+                setAccountType(userData.accountType);
+                setBankAccount(userData.bankAccount);
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        };
+
+        fetchUserData();
+    }, []); // Empty dependency array ensures the effect runs only once on mount
+
+    const handleSave = async () => {
+        try {
+            // TODO: link with backend to update user data
+            const response = await fetch('/api/user', {
+                method: 'PUT', // or 'POST' based on your API
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username,
+                    email,
+                    accountType,
+                    bankAccount,
+                }),
+            });
+
+            if (response.ok) {
+                alert('Settings saved!');
+            } else {
+                alert('Failed to save settings');
+            }
+        } catch (error) {
+            console.error('Error saving user data:', error);
+            alert('Failed to save settings');
+        }
+    };
+
+    const handleDeleteAccount = async () => {
+        try {
+            // TODO: link with backend to delete user account
+            const response = await fetch('/api/user', {
+                method: 'DELETE',
+            });
+
+            if (response.ok) {
+                alert('Account deleted!');
+            } else {
+                alert('Failed to delete account');
+            }
+        } catch (error) {
+            console.error('Error deleting user account:', error);
+            alert('Failed to delete account');
+        }
+    };
+
+    return (
+        <div style={styles.container}>
+            <h2>Settings</h2>
+            <label>
+                Update Username:
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+            </label>
+            <label>
+                Password: {/* Consider not displaying the password for security reasons */}
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </label>
+            <label>
+                Email:
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </label>
+            <label>
+                Account Type:
+                <select value={accountType} onChange={(e) => setAccountType(e.target.value)}>
+                    <option value="normal">Normal</option>
+                    <option value="corporate">Corporate</option>
+                </select>
+            </label>
+            <label>
+                Bank Account:
+                <input type="text" value={bankAccount} onChange={(e) => setBankAccount(e.target.value)} />
+            </label>
+            <button onClick={handleSave}>Save</button>
+            <button onClick={handleDeleteAccount}>Delete Account</button>
+        </div>
+    );
+};
+
+const styles = {
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: '#ADD8E6', // light blue
+        height: '100vh',
+        justifyContent: 'center',
+    },
+};
+
+export default Settings;
