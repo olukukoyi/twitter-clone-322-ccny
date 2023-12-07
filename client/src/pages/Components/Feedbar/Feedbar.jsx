4c6d968ddc8c0ab6fcae07a4ad7b5faaaa393a05
Ramim Tarafdar <rtarafdar96@gmail.com>
2023-12-06 23:40:16 -0500

    Updated show all posts

--- client/src/pages/Components/Feedbar/Feedbar.jsx
+++ client/src/pages/Components/Feedbar/Feedbar.jsx
@@ -9,8 +9,27 @@ const Feedbar = () => {
     const [newPostTitle, setNewPostTitle] = useState('');
     const [newPostContent, setNewPostContent] = useState('');
     const [image, setImage] = useState(null);
+    const [post, setPosts] = useState([]);
+
 
     const userId = Cookies.get('userid');
+
+    // useEffect(() => {
+    //     const fetchPosts = async () => {
+    //         try {
+    //             const response = await fetch('http://localhost:8001/post');
+    //             if (!response.ok) {
+    //                 throw new Error(`HTTP error! Status: ${response.status}`);
+    //             }
+    //             const data = await response.json();
+    //             setPosts(data.posts);
+    //         } catch (error) {
+    //             console.error("Error fetching posts:", error.message);
+    //         }
+    //     };
+
+    //     fetchPosts();
+    // }, []);
 
     const handlePostSubmit = async (e) => {
         e.preventDefault();
@@ -62,14 +81,18 @@ const Feedbar = () => {
                         </label> */}
                         <button type="submit">Post</button>
                     </div>
-                    
                 </form>
             </div>
+            {/* <div className='posts-display'>
+                {posts.map(post => (
+                    <div key={post.id} className="post-item">
+                        <h3>{post.title}</h3>
+                        <p>{post.body}</p>
+                    </div>
+                ))}
+            </div> */}
         </div>
     );
 };
-
-
-
 
 export default Feedbar;
